aaf486816139aef6a197c3e1a75dc2879e73e742
anusika <anusikanijher@gmail.com>
2020-11-06 19:36:57 -0800

    formatting functions and adding comments

--- acnlPath.py
+++ acnlPath.py
@@ -3,40 +3,54 @@ import cv2
 import numpy as np
 import win32gui
 import itertools
+import random
+import argparse
 
+
+#contains all coordiantes for path
 path = []
+#name of tool/window for streaming from 3ds
+name = "Snickerstream"
 
-def windowCallback(hwnd, hwndFound, name="Snickerstream"):
-
+def windowCallback(hwnd, hwndFound, name = name):
+    """
+    callback function for getWindow
+    check if hwnd found matches 3ds streaming window
+    """
     if name in win32gui.GetWindowText(hwnd):
         hwndFound.append(hwnd)
     return True
 
 def getWindow():
+    """
+    get window size of 3ds streaming window
+    """
     hwndFound = []
     win32gui.EnumWindows(windowCallback, hwndFound)
     return win32gui.GetWindowRect(hwndFound[0])
 
 
-windowSize = getWindow()
-print(windowSize)
-halfheight = (windowSize[3] - windowSize[1])/2
-bottomScreen = (windowSize[0], windowSize[1] + halfheight, windowSize[2], windowSize[3])
-topScreen = (windowSize[0], windowSize[1], windowSize[2], windowSize[3] - halfheight)
+def getPoints():
 
-def main():
+    windowSize = getWindow()
+
     while True:
+        #get image of window and convert to RGB
         screen = np.array(ImageGrab.grab(windowSize, all_screens=True))
         RGB_img = cv2.cvtColor(screen, cv2.COLOR_BGR2RGB)
 
+        #get height and width of window to select bottom screen of 3ds
         height, width, channels = RGB_img.shape
-
         bottomImg = RGB_img[round((height/2)):height, 0:width]
 
+        #create a mask that picks only the red pixels in a image
+        #this is because the location of your character is in a red shape
         lower = np.array([0, 0, 200])
         upper = np.array([0, 0, 255])
         shapeMask = cv2.inRange(bottomImg, lower, upper)
 
+        #check for the first white pixel in the mask that represents your location
+        #if a white pixel can't be found add a placeholder of (0, 0)
         whitePixels = np.array(np.where(shapeMask == 255))
 
         if whitePixels.size == 0:
@@ -45,9 +59,10 @@ def main():
             center = whitePixels[:,0] 
             center = (center[1], center[0])
 
-        print(center)
+        #add white pixel location to path
         path.append(center)
 
+        #add a green circle of your current location on the image
         radius = 10
         color = (0, 255, 0)
         thickness = 2
@@ -56,22 +71,53 @@ def main():
 
         cv2.imshow('Python Window', image)
 
+        #if 'q' is pressed end the sampling
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             return image
             break
 
+def createPathImage(image):
+    """
+    creates a red path line based on cooridates in path list
+    """
+    #remove consecutive duplicates to reduce drawing time
+    #remove placeholder (0,0)
+    global path
+    path = [corr for corr, _ in itertools.groupby(path)]
+    path = [corr for corr in path if corr != (0 , 0)]
+    #draw coordinate as a red circle
+    for corr in path:
+        cv2.drawMarker(image, (corr[0], corr[1]),(0,0,255), markerType=cv2.MARKER_CROSS, 
+        markerSize=2, thickness=2, line_type=cv2.LINE_AA)
+    cv2.imshow('Python Window', image)
+    cv2.waitKey()
+    return image
 
 
+def saveImg(image):
+    number = str(random.randint(1111,9999))
+    filename = "ACNL" + number + ".jpg"
+    cv2.imwrite(filename, image)
 
-image = main()
 
-path = [corr for corr, _ in itertools.groupby(path)]
-path = [corr for corr in path if corr != (0 , 0)]
-print(path)
+def main():
+    #pick a different streaming client besides SnickerStream
+    parser = argparse.ArgumentParser(description='Create a Path :)')
+    parser.add_argument('--stream', dest='stream', action='store', default=False,
+                    help='name of streaming window')
+    args = parser.parse_args()
+    if args.stream:
+        global name
+        name = args.stream
 
-for corr in path:
-    cv2.drawMarker(image, (corr[0], corr[1]),(0,0,255), markerType=cv2.MARKER_CROSS, 
-    markerSize=2, thickness=2, line_type=cv2.LINE_AA)
-cv2.imshow('Python Window', image)
-cv2.waitKey(0) 
+    #first get the path
+    image = getPoints()
+    #create path drawing
+    image = createPathImage(image)
+    #save image as .jpeg
+    saveImg(image)
+
+
+if __name__ == "__main__":
+    main()
